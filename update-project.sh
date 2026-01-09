b47076550a953308fb68d91c47f7bbe077faa75d
Sonic_Shulk_100 <romain.mourouvin2004@gmail.com>
2026-01-09 18:08:03 +0100

    Modified the shell script.

--- update-project.sh
+++ update-project.sh
@@ -7,7 +7,7 @@ git switch main && git pull && git rebase origin/main && git push -u origin main
 #==============
 # Main Branches
 #==============
-MAIN_BRANCHES=("script", "images", "audio")
+MAIN_BRANCHES=("script" "images" "audio")
 
 for MAIN_BRANCH in "${MAIN_BRANCHES[@]}"; do
   git switch "$MAIN_BRANCH"
@@ -19,7 +19,7 @@ done
 #======================
 # Sub Branches : SCRIPT
 #======================
-SCRIPT_BRANCHES=("script-characters", "script-narration", "script-choices")
+SCRIPT_BRANCHES=("script-characters" "script-narration" "script-choices")
 
 for SUB_SCRIPT_BRANCH in "${SCRIPT_BRANCES[@]}"; do
   git switch "$SUB_SCRIPT_BRANCH"
@@ -32,7 +32,7 @@ done
 #=====================
 # Sub Branches : IMAGE
 #=====================
-IMAGE_BRANCHES=("images-characters", "images-backgrounds")
+IMAGE_BRANCHES=("images-characters" "images-backgrounds")
 
 for SUB_IMAGE_BRANCH in "${IMAGE_BRANCHES[@]}"; do
   git switch "$SUB_IMAGE_BRANCH"
