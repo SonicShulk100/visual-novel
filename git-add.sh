3822fd3de0dbbb48ae7a78aefa957ea0d9e30eee
rmourouvin <romain.mourouvin2004@gmail.com>
2026-01-16 15:35:36 +0100

    Deleted the shell scripts to merge into one.

--- git-add.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-fortune | cowsay -f dragon -b
-
-git add .
-git commit -m "$1"
-git push origin "$2"
--- rebase.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-git switch "$1"
-
-git pull
-
-git rebase "origin/$2"
-
-git push -u origin "$1"
