77564d520bf1ba4c35358c667ffdf81c4b89374d
rmourouvin <romain.mourouvin2004@gmail.com>
2026-01-27 14:01:45 +0100

    "Added the update."

--- project.sh
+++ project.sh
@@ -32,12 +32,74 @@ case $command in
                 cowsay -d "Enter either the message or the branch."
                 exit 1
             fi
+            git fetch origin
             git switch "$targetBranch"
             git pull
             git rebase origin/"$sourceBranch"
             git push -u origin "$targetBranch"
 
             cowsay -g "Rebasing complete."
+        } || {
+            cowsay -d "Something went wrong...!"
+            exit 1
+        }
+        ;;
+    (update)
+        {
+            git fetch origin
+            
+            git switch main
+            git pull
+
+            #==============
+            # Main Branches
+            #==============
+
+            MAIN_BRANCHES=("gui" "script" "audio" "images")
+            for MAIN_BRANCH in "${MAIN_BRANCHES[@]}"; do
+                git switch "$MAIN_BRANCH"
+                git pull
+                git rebase origin/main
+                git push -u origin "$MAIN_BRANCH"
+            done
+
+            #======================
+            # Sub-Branches : Script
+            #======================
+            BRANCHES_SCRIPT=("script-characters" "script-choices" "script-narration")
+            for BRANCH_SCRIPT in "${BRANCHES_SCRIPT[@]}"; do
+                git switch "$BRANCH_SCRIPT"
+                git pull
+                git rebase origin/main
+                git push -u origin "$BRANCH_SCRIPT"
+            done
+
+            #=====================
+            # Sub-Branches: Images
+            #=====================
+            BRANCHES_IMAGES=("images-backgrounds" "images-characters")
+            for BRANCH_IMAGE in "${BRANCHES_IMAGES[@]}"; do
+                git switch "$BRANCH_IMAGE"
+                git pull
+                git rebase origin/main
+                git push -u origin "$BRANCH_IMAGE"
+            done
+
+            #====================
+            # Sub-Branches: Audio
+            #====================
+            BRANCHES_AUDIO=("audio-music" "audio-sfx")
+            for BRANCH_AUDIO in "${BRANCHES_AUDIO[@]}"; do
+                git switch "$BRANCH_AUDIO"
+                git pull
+                git rebase origin/main
+                git push -u origin "$BRANCH_AUDIO"
+            done
+
+            git switch main
+            git pull
+
+            cowsay -g "Updating complete."
         } || {
             cowsay -d "Something went wrong...!"
             exit 1
